c01443a9e9899f2ee69ee767e4d4b121f6909e24
KT <krakovszkyt@gmail.com>
2023-12-20 21:51:24 +0100

    initial commit

---
+++ src/main/java/com/kt/gardennews/GardenNewsApplication.java
@@ -0,0 +1,13 @@
+package com.kt.gardennews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kt/gardennews/GardenNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.gardennews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
